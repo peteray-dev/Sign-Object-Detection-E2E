de2681736bfd2bb276bf7567dd007edee415f3a0
peteray-dev <peteraydev@gmail.com>
2025-01-09 01:31:41 +0000

    requirement set

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sign-Object-Detection-E2E
+
+<!-- conda create -n signLang python=3.7 -y -->
+
+<!-- conda activate signLang -->
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'peteray-dev',
+    author_email='popoolaayomide37@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
