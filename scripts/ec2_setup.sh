b9511154700312e9bc7288d2624bbd7ee43555fe
peteray-dev <peteraydev@gmail.com>
2025-01-19 22:26:30 +0000

    CICD added with jenkins

--- scripts/ec2_setup.sh
+++ scripts/ec2_setup.sh
@@ -16,7 +16,11 @@ newgrp docker
 
 sudo apt install awscli -y
 
-
+Alternatively for aws cli installation use ths: 
+sudo apt install curl unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 ## AWS configuration
 
--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -5,7 +5,8 @@ sudo apt update
 sudo apt install openjdk-8-jdk -y
 
 https://pkg.jenkins.io/
-https://pkg.jenkins.io/debian-stable/
+https://pkg.jenkins.io/debian-stable/ # use the code in here if the java cant install directly
+
 
 sudo systemctl start jenkins
 
@@ -29,6 +30,13 @@ sudo usermod -aG docker jenkins
 newgrp docker
 
 sudo apt install awscli -y
+
+Alternatively for aws cli installation use ths: 
+sudo apt install curl unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 
 sudo usermod -a -G docker jenkins
 
