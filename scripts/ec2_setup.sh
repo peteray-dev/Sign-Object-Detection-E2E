19931be5f23d901484510cfd5b44bf19b4701c2d
peteray-dev <peteraydev@gmail.com>
2025-01-20 20:49:29 +0000

    credential changed

--- scripts/ec2_setup.sh
+++ scripts/ec2_setup.sh
@@ -16,7 +16,7 @@ newgrp docker
 
 sudo apt install awscli -y
 
-Alternatively for aws cli installation use ths: 
+# Alternatively for aws cli installation use ths: 
 sudo apt install curl unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
